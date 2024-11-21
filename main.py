a5d844789655f81c661d42a8b8bc3c7a651dfbd4
kago <karen.govaerts@gmail.com>
2024-11-21 23:19:42 +0100

    I built a spaceship today!

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+
+    gameclock = pygame.time.Clock()
+    dt = 0
+
+    # instantiate player object
+    x = SCREEN_WIDTH / 2
+    y = SCREEN_HEIGHT / 2
+    itme = Player(x, y)
 
     while True: 
         for event in pygame.event.get():
@@ -11,7 +20,13 @@ def main():
                     return
                 
         screen.fill("black")
+
+        itme.draw(screen)
+
         pygame.display.flip()
+
+        # limit the timeframe to 60 FPS
+        dt = gameclock.tick(60) / 1000
         
    
 
